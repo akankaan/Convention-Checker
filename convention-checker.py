6cc0bf9e2633e9b74561a512cb9924b7f3e8ffcc
unpicasso <kaan1286@yahoo.com>
2026-02-05 22:35:22 -0500

    Tab check done, indentation in progress

--- convention-checker.py
+++ convention-checker.py
@@ -10,12 +10,12 @@ with open("example.v", "r", encoding="utf-8") as f:
 # based on "module" and "endmodule"
 def find_implementation_range(lines):
     start = 0
-    end = 0
+    end   = 0
     
     for i in range(0, len(lines)):
         if "endmodule" in lines[i]:
             end = i + 1
-        elif "module" in lines[i]:
+        elif "module"  in lines[i]:
             start = i + 1
     
     line_range = [start, end]
@@ -28,7 +28,7 @@ def line_length_check(lines, line_range):
 
     longLineCount = 0
     for i in range(line_range[0], line_range[1]):
-        if(len(lines[i]) > 74):
+        if( len(lines[i]) > 74 ):
             longLineCount = longLineCount + 1
             print("Warning: Line " + str(i) + " is " + 
                   str(len(lines[i])) + " characters long")
@@ -36,4 +36,18 @@ def line_length_check(lines, line_range):
 line_length_check(lines, line_range)
 
 # Convention 2.2: Indentation
+def indentation_check(lines, line_range):
+
+    # Checks for tabs
+    tabCount = 0
+    for i in range(line_range[0], line_range[1]):
+        if "\t" in lines[i]:
+            tabCount = tabCount + 1
+            print("Warning: Line " + str(i) + " contains a tab")
+
+    # Checks for consistent indentation
+
+    # FIXME
+
+indentation_check(lines, line_range)
 
